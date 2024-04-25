c0609c6f2713f1676b44ea141bdfbc57b81cdd70
Breno Garcia <BrenoGHeat@gmail.com>
2024-04-25 14:01:25 -0300

    tarefa 4 começada

--- open-music/src/scripts/index.js
+++ open-music/src/scripts/index.js
@@ -2,10 +2,12 @@
 import {applyInputRangeStyle} from "./inputRange.js";
 import { albumList } from "./albumsDatabase.js";
 import { renderCard } from "./createElements.js";
+import { criaEvento } from "./theme.js";
 
  function routine(){
    applyInputRangeStyle();
    renderCard(albumList);
+   criaEvento();
 
  }
 
---
+++ open-music/src/scripts/theme.js
@@ -0,0 +1,14 @@
+
+
+export function criaEvento(){
+
+const button = document.querySelector(".button-header");
+
+
+button.addEventListener("click", function (){
+       console.log("Estou funcionando")
+    
+});
+};
+
+
