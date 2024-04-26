30e9d4584b1097d159d1e34d02a9d67c87d84f8c
Breno Garcia <BrenoGHeat@gmail.com>
2024-04-26 12:21:29 -0300

    dark-mode add

--- open-music/src/scripts/buttons.js
+++ open-music/src/scripts/buttons.js
@@ -3,6 +3,7 @@ export function butonSelect(){
 
 const buttons = document.querySelectorAll('.button-filter');
 
+
         buttons.forEach(button => {
         button.addEventListener('click', function() {
         buttons.forEach(button => button.classList.remove('selected'));
--- open-music/src/scripts/theme.js
+++ open-music/src/scripts/theme.js
@@ -1,12 +1,26 @@
 
 export function criaEvento(){
 
-const button = document.querySelector(".button-header");
+const button = document.querySelector(".header_theme-btn");
+const divButton = document.querySelector(".tema-button");
+const bodyDark = document.querySelector("body");
+const logoDark = document.querySelector(".header-logo");
+const genreDark = document.querySelector(".title-genre");
+const buttonDark = document.querySelector(".button-filter");
+const titlePriceDark = document.querySelector(".title-price");
+const h1Dark = document.querySelector(".album-title");
 
 
 button.addEventListener("click", function (){
-       console.log("Estou funcionando")
-    
+       button.classList.toggle("header_theme-btn--sun");
+       divButton.classList.toggle("tema-button-dark");
+       bodyDark.classList.toggle("body-dark-mode");
+       logoDark.classList.toggle("header-logo-dark-mode");
+       genreDark.classList.toggle("title-genre-dark-mode");
+       buttonDark.classList.toggle("button-filter-dark-mode");
+       titlePriceDark.classList.toggle("title-price-dark-mode");
+       h1Dark.classList.toggle("album-title-dark-mode");
+
 });
 };
 
